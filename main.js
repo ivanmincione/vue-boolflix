8673764d0e4438d1911d4468467a2ce9142b9b9e
Ivan Mincione <ivan.mincione92@outlook.it>
2020-11-24 16:51:35 +0100

    Add MILESTONE 1

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 var app = new Vue ({
     el: '#root',
     data: {
+        movies:[],
+        filterSearch:'',
 
     },
 
     methods: {
+        btnResearch() {
+            axios.get("https://api.themoviedb.org/3/search/movie" , {
+                params:{
+                    api_key: "2d2a4a44dc798710847157dec0ed05f2",
+                    query: this.filterSearch
+                }
+            })
+            .then((reply) => {
+                this.movies = reply.data.results;
+                this.filterSearch = '';
 
+                console.log(this.movies);
+            })
+        }
     }
 
 
